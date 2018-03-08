9cca6035ec6a473898b3b09850c392b8f27d5a52
彭俊豪 <1208984024@qq.com>
2018-03-08 16:14:59 +0800

    修改描述信息

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = appInfo => {
-    const config = exports = {};
+    const config = {};
 
     // use for cookie sign key, should change to your own and keep security
     config.keys = appInfo.name + '_1516418387158_7965';
