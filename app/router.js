8c5df85f3314d7ddb304b76aaf006b83d4c37ba6
彭俊豪 <1208984024@qq.com>
2018-03-09 15:04:13 +0800

    添加auth中间件

--- app/router.js
+++ app/router.js
@@ -4,7 +4,10 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
+    //用户校验中间件
+    const auth = app.middlewares.auth();
     const {router, controller} = app;
+
     router.get('/', controller.home.index);
     router.post('/api/user', 'api.user.info');
     router.post('/api/insert', 'api.user.insert')
